b26fe6a474199972403cfb3c00e4196f782c8e41
Antonio <tonofalcon22@gmail.com>
2021-05-21 23:05:39 -0500

    primer portafolio web

---
+++ js/scroll/navegacionScroll.js
@@ -0,0 +1,29 @@
+/*=============================================
+NAVEGACIÓN SCROLL
+=============================================*/
+
+$(".nav-link").click(function(e){
+
+	e.preventDefault();
+
+	var target = $(this).attr("href");
+	
+	$("html, body").animate({
+
+		scrollTop: $(target).offset().top
+
+	},1000, "easeOutBack")
+
+})
+
+/*=============================================
+SCROLL UP
+=============================================*/
+
+$(function (){
+		$.scrollUp({
+			scrollText: "",
+			scrollSpeed:2000,
+			easingType: "easeOutQuint"
+		})
+	})
